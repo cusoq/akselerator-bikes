20abffdc75ee7fc7b5c73abe24da18bcfaceab92
cusoq <cusoq@mail.ru>
2021-06-13 21:55:59 +0300

    Add adaptive header, modal navigation, navigation script

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,4 @@
-// 'use strict';
+'use strict';
 // var pageHeader = document.querySelector('.page-header');
 // var headerToggle = document.querySelector('.page-header__toggle');
 
@@ -13,3 +13,63 @@
 //     pageHeader.classList.remove('page-header--opened');
 //   }
 // });
+
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+var popupOverlay = document.querySelector('.navigation');
+// console.log(popupOverlay);
+var crossButton = popupOverlay.querySelector('.navigation__menu-close-cross-button');
+var navLink = popupOverlay.querySelectorAll('.navigation__link');
+// console.log(crossButton);
+var burgerButton = document.querySelector('.intro__menu-open-burger-button');
+// console.log(burgerButton);
+
+// открытие модального окна
+var showPopup = function () {
+  popupOverlay.classList.remove('navigation--closed');
+  crossButton.classList.remove('navigation__menu-close-cross-button--closed');
+  burgerButton.classList.add('intro__menu-open-burger-button--closed');
+  // checkStorageSupport();
+  // getFocus();
+};
+// закрытие модального окна
+var closePopup = function () {
+  popupOverlay.classList.add('navigation--closed');
+  crossButton.classList.add('navigation__menu-close-cross-button--closed');
+  burgerButton.classList.remove('intro__menu-open-burger-button--closed');
+};
+
+// Хендлеры
+var onClickOpener = function (evt) {
+  evt.preventDefault();
+  showPopup();
+};
+
+var onClickCloser = function (evt) {
+  evt.preventDefault();
+  closePopup();
+};
+
+var onEnterOpener = function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    evt.preventDefault();
+    showPopup();
+  }
+};
+
+var onEscCloser = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    evt.preventDefault();
+    window.removeEventListener('keydown', onEscCloser);
+    closePopup();
+  }
+};
+
+// Обработчики событий
+burgerButton.addEventListener('click', onClickOpener);
+crossButton.addEventListener('keydown', onEnterOpener);
+crossButton.addEventListener('click', onClickCloser);
+navLink.forEach(function (element) {
+  element.addEventListener('click', onClickCloser);
+});
+document.addEventListener('keydown', onEscCloser);
